991b0a5d236493aee45cb5841e233fb029100171
trevor <me@trevorbehnke.com>
2021-09-21 14:19:12 -0700

    feat: :art: fixed tag overflow to wrap

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -20,16 +20,16 @@
 
 <div class="py-4 grid gap-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
 	{#each posts as { title, slug, excerpt, coverImage, tags }}
-		<div class="card text-center shadow-2xl mb-20 ">
+		<div class="card text-center shadow-2xl">
 			<figure class="px-10 pt-10">
 				<img class="rounded-xl" src={coverImage.url} alt={`Cover image for ${title}`} />
 			</figure>
 			<div class="card-body">
 				<h2 class="card-title">{title}</h2>
 				<p class="excerpt">{excerpt}</p>
-				<div class="flex justify-center mt-5 space-x-2">
+				<div class="flex flex-wrap justify-center mt-5 space-x-2">
 					{#each tags as tag}
-						<span class="badge badge-primary">{tag}</span>
+						<span class="badge badge-primary mt-3">{tag}</span>
 					{/each}
 				</div>
 				<div class="justify-center card-actions">
