952e4a5cda5a4ae1924e9549da509809ce365824
trevor <me@trevorbehnke.com>
2021-09-21 01:24:03 -0700

    switched to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,25 @@
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: adapter(),
+		prerender: {
+			crawl: true,
+			enabled: true,
+			onError: 'continue',
+			pages: ['*']
+		}
+	},
+
 	preprocess: [
 		preprocess({
 			postcss: true
 		})
-	],
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: vercel()
-	}
+	]
 };
 
 export default config;
