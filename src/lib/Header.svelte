57713c3f2d25e6c19d14f71c704e97ee58876682
trevor <me@trevorbehnke.com>
2021-09-21 21:53:18 -0700

    configured darkmode functionality

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,10 +1,12 @@
 <script>
 	import { page } from '$app/stores';
-	// import ThemeToggle from './themeToggle.svelte';
+	export let dark;
 </script>
 
 <nav class="backdrop-filter backdrop-blur py-10 fixed w-full z-10 pin-t">
-	<div class=" flex justify-between flex-row max-w-6xl container mx-auto px-6">
+	<div
+		class="bg-gray-300 dark:bg-gray-900 flex justify-between flex-row max-w-6xl container mx-auto px-6"
+	>
 		<div class="w-12 h-12">
 			<a rel="prefetch" href="/"
 				><img class="object-contain" src="./t-block-light.png" alt="trevor-logo" /></a
@@ -25,7 +27,14 @@
 					<a rel="prefetch" class="mx-4 text-lg" href="/blog">Blog</a>
 				</li>
 				<li>
-					<!-- <ThemeToggle /> -->
+					<div class="flex-0">
+						<a
+							href="/"
+							on:click|preventDefault={() => {
+								dark = !dark;
+							}}>{dark ? 'dark' : 'light'}</a
+						>
+					</div>
 				</li>
 			</ul>
 		</div>
