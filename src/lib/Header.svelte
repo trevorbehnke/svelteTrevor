27ec38073bcff7fc6d5e905777555ad6f839186f
trevor <me@trevorbehnke.com>
2021-09-21 17:12:52 -0700

    feat: :art: added fixed navbar w blur

---
+++ src/lib/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { page } from '$app/stores';
+	// import ThemeToggle from './themeToggle.svelte';
+</script>
+
+<nav class="backdrop-filter backdrop-blur p-2 mt-0 fixed w-full z-10 pin-t">
+	<ul class="flex justify-center w-full">
+		<li class:active={$page.path === '/'}>
+			<a class="mx-4 text-lg" href="/">Home</a>
+		</li>
+		<li class:active={$page.path === '/about'}>
+			<a class="mx-4 text-lg" href="/about">About</a>
+		</li>
+		<li class:active={$page.path === '/blog'}>
+			<a class="mx-4 text-lg" href="/blog">Blog</a>
+		</li>
+		<li>
+			<!-- <ThemeToggle /> -->
+		</li>
+	</ul>
+</nav>
+
+<style>
+	.active {
+		border-bottom: 2px solid red;
+	}
+</style>
